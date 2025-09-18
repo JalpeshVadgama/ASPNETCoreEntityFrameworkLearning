0c7d814b72309ac59b897b6a0ca749a3a462b88c
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2025-09-18 23:01:47 +0530

    aspnetcore demo

---
+++ ASPNetCoreWithEFDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetCoreWithEFDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
